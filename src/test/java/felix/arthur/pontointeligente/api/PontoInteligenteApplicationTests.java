f61f88c9978573dac836ffd29dd9c4d81e22cca7
ArthurUFelix <arthurufelix@gmail.com>
2020-05-17 19:12:58 -0300

    Commit inicial dos arquivos do projeto

---
+++ src/test/java/felix/arthur/pontointeligente/api/PontoInteligenteApplicationTests.java
@@ -0,0 +1,13 @@
+package felix.arthur.pontointeligente.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontoInteligenteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
