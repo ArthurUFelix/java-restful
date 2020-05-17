04a26dfd7ed52bf3b2e15d134bb3eed6a4748f52
ArthurUFelix <arthurufelix@gmail.com>
2020-05-17 20:56:45 -0300

    Adicionado entidades, enums e flyway para versionamento do banco de dados

---
+++ src/main/java/felix/arthur/pontointeligente/api/entities/Empresa.java
@@ -0,0 +1,95 @@
+package felix.arthur.pontointeligente.api.entities;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "empresa")
+public class Empresa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "razao_social", nullable = false)
+    private String razaoSocial;
+
+    @Column(name = "cnpj", nullable = false)
+    private String cnpj;
+
+    @Column(name = "data_criacao", nullable = false)
+    private Date dataCriacao;
+
+    @Column(name = "data_atualizacao", nullable = false)
+    private Date dataAtualizacao;
+
+    @OneToMany(mappedBy = "empresa", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<Funcionario> funcrionarios;
+
+    @PreUpdate
+    public void preUpdate() {
+        dataAtualizacao = new Date();
+    }
+
+    @PrePersist
+    public void prePersist() {
+        final Date atual = new Date();
+        dataCriacao = atual;
+        dataAtualizacao = atual;
+    }
+
+    @Override
+    public String toString() {
+        return "Empresa [id=" + id + ", razaoSocial=" + razaoSocial + ", cnpj=" + cnpj + ", dataCriacao=" + dataCriacao
+                + ", dataAtualizacao=" + dataAtualizacao + "]";
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getRazaoSocial() {
+        return razaoSocial;
+    }
+
+    public void setRazaoSocial(String razaoSocial) {
+        this.razaoSocial = razaoSocial;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public Date getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(Date dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    public Date getDataAtualizacao() {
+        return dataAtualizacao;
+    }
+
+    public void setDataAtualizacao(Date dataAtualizacao) {
+        this.dataAtualizacao = dataAtualizacao;
+    }
+
+    public List<Funcionario> getFuncrionarios() {
+        return funcrionarios;
+    }
+
+    public void setFuncrionarios(List<Funcionario> funcrionarios) {
+        this.funcrionarios = funcrionarios;
+    }
+}
---
+++ src/main/java/felix/arthur/pontointeligente/api/enums/PerfilEnum.java
@@ -0,0 +1,6 @@
+package felix.arthur.pontointeligente.api.enums;
+
+public enum PerfilEnum {
+    ROLE_ADMIN,
+    ROLE_USUARIO
+}
